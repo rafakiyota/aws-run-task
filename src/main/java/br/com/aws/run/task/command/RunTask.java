b1909df342166391cffacb4d3fc6930d6c4cb32a
unknown <rfkiyota@gmail.com>
2021-03-10 14:51:45 -0300

    criacao de serviço de task e ajuste no dockerfile

--- src/main/java/br/com/aws/run/task/command/RunTask.java
+++ src/main/java/br/com/aws/run/task/command/RunTask.java
@@ -4,23 +4,25 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-import com.amazonaws.services.ecs.AmazonECS;
-import com.amazonaws.services.ecs.AmazonECSClientBuilder;
-import com.amazonaws.services.ecs.model.DescribeTaskDefinitionRequest;
 import com.amazonaws.services.ecs.model.DescribeTaskDefinitionResult;
-import com.amazonaws.services.ecs.model.RunTaskRequest;
 import com.amazonaws.services.ecs.model.RunTaskResult;
+
+import br.com.aws.run.task.service.EcsTaskService;
 
 @Order(1)
 @Component
 public class RunTask implements ApplicationRunner {
 
 	private static final Logger logger = LoggerFactory.getLogger(RunTask.class);
+	
+	@Autowired
+	private EcsTaskService ecsTaskService;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -42,25 +44,27 @@ public class RunTask implements ApplicationRunner {
 		logger.info("task: {}", task); 
 		
     	if (args.containsOption("type") && "describe-task".equals(args.getOptionValues("type").get(0))) {
-	    	AmazonECS client = AmazonECSClientBuilder.standard().build();
 	    	
-	    	DescribeTaskDefinitionRequest requestTask = new DescribeTaskDefinitionRequest().withTaskDefinition(task);
-	    	DescribeTaskDefinitionResult describeTask = client.describeTaskDefinition(requestTask);
+	    	DescribeTaskDefinitionResult describeTask = ecsTaskService.describeTask(task);
 			
 	    	logger.info("type: {}", args.getOptionValues("type").get(0)); 
 	    	logger.info("describe-task: {}", describeTask);
 		}
     	if (args.containsOption("type") && "run-task".equals(args.getOptionValues("type").get(0))) {
-    		AmazonECS client = AmazonECSClientBuilder.standard().build();
     		
-    		RunTaskRequest request = new RunTaskRequest()
-    				.withCluster(cluster)
-                    .withTaskDefinition(task)
-                    .withLaunchType("FARGATE");
+    		if (!args.containsOption("subnetids") || args.getOptionValues("subnetids").get(0).isEmpty()) {
+        		throw new IllegalArgumentException("argumento subnetids não enviado!!!");
+        	}
+    		if (!args.containsOption("securitygroupids") || args.getOptionValues("securitygroupids").get(0).isEmpty()) {
+    			throw new IllegalArgumentException("argumento securitygroupids não enviado!!!");
+    		}
     		
-    		RunTaskResult taskResult = client.runTask(request);
+    		String subnetids = args.getOptionValues("subnetids").get(0);
+    		String securitygroupids = args.getOptionValues("securitygroupids").get(0);
     		
-    		logger.info("type: {}", args.getOptionValues("type").get(0)); 
+    		RunTaskResult taskResult = ecsTaskService.runTask(cluster, task, subnetids, securitygroupids);
+    		
+    		logger.info("type: {}", args.getOptionValues("type").get(0));
     		logger.info("run-task: {}", taskResult);
     	}    	
     }
